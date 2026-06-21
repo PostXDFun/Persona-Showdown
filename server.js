dd04e81cf092f912dd2aee5782b8545ce98d6fb8
PostXDFun <israelitog2.0@gmail.com>
2026-06-20 22:58:52 -0400

    Actualizada interfaz de Showdown online

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const http = require('http');
 const path = require('path');
 const { WebSocketServer } = require('ws');
 
-const app = report = express();
+const app = express();
 const PORT = process.env.PORT || 8080;
 
 app.use(express.static(path.join(__dirname, 'public')));
